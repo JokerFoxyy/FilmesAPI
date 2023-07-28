7f87e24f88e11104757a9dd762a3a9a5e4ba8e43
Joker <victorassagra@hotmail.com>
2023-07-27 22:04:03 -0300

    first commit

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="A Duração do filme é obrigatória!")]
+        [Range(70,600,ErrorMessage ="A duração deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+
+        [Required(ErrorMessage = "O Titulo do filme é obrigatório!")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Gênero do filme é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+    }
+}
